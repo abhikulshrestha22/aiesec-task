d284bc55088f5204a33b46914c84e7d96b4b9f10
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2018-06-26 13:27:11 +0530

    url changed

--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ class App extends Component {
 
     
     // Fetching the oppourtunity 6124 from api
-    fetch('http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/6124?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c',obj)
+    const url = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/6124?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c'
+    fetch(url,obj)
     .then(
       (res)=>res.json())
     .catch((err)=>console.log(err))
