c1e82744670923e169dc16b1694186edc687b66b
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2018-06-26 13:04:52 +0530

    catch added to fetch  promises

--- src/App.js
+++ src/App.js
@@ -36,14 +36,16 @@ class App extends Component {
     fetch('http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/6124?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c',obj)
     .then(
       (res)=>res.json())
+    .catch((err)=>console.log(err))
     .then((resJson)=>{
       console.log(resJson);
       this.setState({
         opp:resJson,
         oppLocal:Object.assign({},resJson)
-      });
+      })
+      ;
 
-    })
+    }).catch((err)=>console.log(err));
 
               }
 
