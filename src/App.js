eeaeae12e36eefa0f8578d1a569094f4a5607d56
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2018-06-27 15:46:33 +0530

    dates patch fixed

--- src/App.js
+++ src/App.js
@@ -15,12 +15,7 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    var obj = {
-      method:'GET',
-      headers:{
-        'Accept': 'application/json',
-         }
-        }
+    
     
 
     // Initial state
@@ -31,26 +26,35 @@ class App extends Component {
                 };
           
 
+   this.getDataFromApi();
     
-    // Fetching the oppourtunity 6124 from api
-    const url = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/6124?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c'
-    fetch(url,obj)
-    .then(
-      (res)=>res.json())
-    .catch((err)=>console.log(err))
-    .then((resJson)=>{
-      console.log(resJson);
-      this.setState({
-        opp:resJson,
-        oppLocal:Object.assign({},resJson)
-      })
-      ;
+    
+  }
 
-    }).catch((err)=>console.log(err));
-
+  getDataFromApi = ()=>{
+    var obj = {
+      method:'GET',
+      headers:{
+        'Accept': 'application/json',
+         }
+        }
+                // Fetching the oppourtunity 6124 from api
+              const url = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/6124?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c'
+              fetch(url,obj)
+              .then(
+                (res)=>res.json())
+              .catch((err)=>console.log(err))
+              .then((resJson)=>{
+                console.log(resJson);
+                this.setState({
+                  opp:resJson,
+                  oppLocal:Object.assign({},resJson)
+                })
+                ;
+          
+              }).catch((err)=>console.log(err));
+          
               }
-
-    
   
     
 
@@ -91,17 +95,19 @@ class App extends Component {
     saveOpp=()=>{
      
       console.log("in func saveOpp");
+      console.log("local app close date");
+      console.log(this.state.oppLocal.applications_close_date);
       const url = 'http://gisapi-web-staging-1636833739.eu-west-1.elb.amazonaws.com/v2/opportunities/6124?access_token=dd0df21c8af5d929dff19f74506c4a8153d7acd34306b9761fd4a57cfa1d483c'
       const param_update_title = "opportunity[title]=" + this.state.oppLocal.title;
-      const param_update_applications_close_date="opportunity[applications_close_date]" + this.state.oppLocal.applications_close_date;
-      const param_update_earliest_start_date="opportunity[earliest_start_date]"+ this.state.oppLocal.earliest_start_date;
-      const param_update_latest_end_date="opportunity[latest_end_date]" + this.state.oppLocal.latest_end_date;
-      const param_update_description="opportunity[description]"+this.state.oppLocal.description
-      const param_update_salary="opportunity[specifics_info][salary]"+ this.state.oppLocal.specifics_info.salary;
+      const param_update_applications_close_date="opportunity[applications_close_date]=" + this.state.oppLocal.applications_close_date;
+      const param_update_earliest_start_date="opportunity[earliest_start_date]="+ this.state.oppLocal.earliest_start_date;
+      const param_update_latest_end_date="opportunity[latest_end_date]=" + this.state.oppLocal.latest_end_date;
+      const param_update_description="opportunity[description]="+this.state.oppLocal.description
+      const param_update_salary="opportunity[specifics_info][salary]="+ this.state.oppLocal.specifics_info.salary;
 
       fetch(url + '&' + param_update_title + '&' + param_update_applications_close_date 
-              + param_update_earliest_start_date + param_update_latest_end_date
-              + param_update_description + param_update_salary ,
+              +'&'+ param_update_earliest_start_date + '&'+param_update_latest_end_date
+              +'&'+ param_update_description + '&'+param_update_salary ,
       {
         method:'PATCH',
         headers:{
@@ -114,7 +120,9 @@ class App extends Component {
         .then((resJson)=>{
           console.log("patch done");
           console.log(resJson);
-        });
+        })
+        .then(()=>(this.getDataFromApi()))
+        ;
         
 
 
